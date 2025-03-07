b3bddbca1a8787ad7ec64ebda745eff8e0d69a71
Dipesh-Chaudhary <99006857+Dipesh-Chaudhary@users.noreply.github.com>
2025-03-07 15:11:00 +0545

    Update main.py

--- main.py
+++ main.py
@@ -113,4 +113,7 @@ async def predict(file: UploadFile = File(...)):
 
     # Return the caption and the detailed response
     return PredictionResponse(caption=caption, response=response)
-
+#It is used since i have to access it through kaggle
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
